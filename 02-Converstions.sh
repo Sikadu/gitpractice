f5ab9aa966f713445dabe4ac113e30db5b575a36
Sindhura <sindhu.tiruvedula@gmail.com>
2024-09-03 22:14:42 +0530

    created new conversations

---
+++ 02-Converstions.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo Ramesh: Hi Suresh
+echo Suresh: Hi Ramesh, How are you ?
+echo Ramesh: I'm good thanks, How about you ?
+echo Suresh: I'm good too. What are you doing now ?
+echo Ramesh: Suresh..currently i'm learing devops
+echo suresh: oh! whats the institue name ?
+echo Rameesh: Joindevops.com  
