c03c3af7f125b7ada08de67bc3ed6e9bed5d6384
Sindhura <sindhu.tiruvedula@gmail.com>
2024-09-04 23:03:09 +0530

    updated

--- 09-sepcial-vars.sh
+++ 09-sepcial-vars.sh
@@ -4,4 +4,5 @@ echo "allow all the variables to the script: $@"
 echo "number of variables passed: $#"
 echo "name of the script: $0"
 echo "current working directory: $PWD" #CAPS
-echo "home directory of current user: $home
+echo "home directory of current user: $HOME"
+echo "PID of the script: $$"
