0dcbbc9f0648c1472500b0162939e677e36141b4
Sindhura <sindhu.tiruvedula@gmail.com>
2024-09-05 19:22:57 +0530

    updated

--- 11.mysql_installation.sh
+++ 11.mysql_installation.sh
@@ -14,3 +14,16 @@ else
 echo " user have root access "
 
 fi
+
+dnf list installed mysql
+
+if [[ $? -ne 0 ]]
+
+then
+    echo " please install mysql "
+    dnf install mysql -y
+    else
+    echo " my sql not installed "
+    exit 1
+
+fi
