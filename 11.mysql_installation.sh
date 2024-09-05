08028ec427e47488a07f64b7f8ec4dbd6d1906cc
Sindhura <sindhu.tiruvedula@gmail.com>
2024-09-05 19:28:29 +0530

    updated

--- 11.mysql_installation.sh
+++ 11.mysql_installation.sh
@@ -1,25 +1,53 @@
 #!/bin/bash
 
+# USERID=$(id -u)
+
+# if [[ $USERID -ne 0 ]]
+
+# then 
+
+# echo " user have root access "
+
+# exit 1
+
+# fi
+
+# dnf list installed git
+
+# if [[ $? -ne 0 ]]
+# then
+#     echo " please install git "
+#     dnf install git -y
+#     else
+#     echo " my git not installed "
+#     exit 1
+
+# fi
+
+#!/bin/bash
+
 USERID=$(id -u)
+#echo "User ID is: $USERID"
 
-if [[ $USERID -ne 0 ]]
-
-then 
-
-echo " user have root access "
-
-exit 1
-
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root priveleges"
+    exit 1
 fi
 
-dnf list installed git
+dnf list installed git # Just checking whether installed or not
 
-if [[ $? -ne 0 ]]
+if [ $? -ne 0 ]
 then
-    echo " please install git "
-    dnf install git -y
+    echo "Git is not installed, going to install it.."
+    dnf install git -y # here installing
+    if [ $? -ne 0 ]
+    then
+        echo "Git installation is not success...check it"
+        exit 1
     else
-    echo " my git not installed "
-    exit 1
-
+        echo "Git installation is success"
+    fi
+else
+    echo "Git is already installed, nothing to do.."
 fi
