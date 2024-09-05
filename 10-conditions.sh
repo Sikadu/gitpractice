3ede27abd0d219c3ef1b53bf30d9586055b875c8
Sindhura <sindhu.tiruvedula@gmail.com>
2024-09-05 17:18:23 +0530

    updated

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 
-#NUMBER=$1
-
-#if [ $NUMBER -gt 20 ]
-#then
-#    echo "given number: $NUMBER is greater than 20"
-#else
-#    echo "given number: $NUMBER is less than 20"
-#fi
-
-
 NUMBER=$1
 
-if [[ $NUMBER -gt 20 ]] #gt, lt, eq, -ne, -ge, -le
+if [ $NUMBER -gt 20 ]
 then
-    echo "Given number: $NUMBER is greater than 20"
+   echo "given number: $NUMBER is greater than 20"
 else
-    echo "Given number: $NUMBER is less than 20"
+    echo "given number: $NUMBER is less than 20"
 fi
