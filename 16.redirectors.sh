a704548f7691cd6e097fb9a06c1b61d8beedd894
Sindhura <sindhu.tiruvedula@gmail.com>
2024-09-06 23:13:04 +0530

    updated

--- 16.redirectors.sh
+++ 16.redirectors.sh
@@ -1,50 +1,64 @@
 #!/bin/bash
 
-LOG_FOLDER="\var\log\shell_script"
+LOGS_FOLDER="\var\log\shell_script"
 SCRIPT_NAME=$(echo $0 | cut -d '.' -f1)
-DATE=$(date +%Y-%m-%d-%H-%M-%S)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE=$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log
+mkdir -p $LOGS_FOLDER
 
-# R="\e[31m"
-# G="\e[32m"
-# Y="\e[33m"
-# N="\e[0m"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
-# USERID=$(id -u)
+USERID=$(id -u)
 
-# CHECK_ROOT()
-# {
-#     if [[ $USERID -ne 0 ]]
-#     then 
-#     echo -e " $G Please run the script with root access $N"
-#     exit 1
-#     fi
-# }
+CHECK_ROOT()
+{
+    if [[ $USERID -ne 0 ]]
+    then 
+    echo -e " $G Please run the script with root access $N" | tee -a $LOG_FILE
+    exit 1
+    fi
+}
 
-# VALIDATE()
-# {
-#     if [[ $1 -ne 0 ]]
-#         then 
-#         echo -e " $2 its $R failed $N "
-#         exit 1
-#         else
-#         echo -e " $2 its $G success $N "
-#      fi   
+VALIDATE()
+{
+    if [[ $1 -ne 0 ]]
+        then 
+        echo -e " $2 its $R failed $N " | tee -a $LOG_FILE
+        exit 1
+        else
+        echo -e " $2 its $G success $N " | tee -a $LOG_FILE
+     fi   
         
-# }
+}
 
-# CHECK_ROOT
+USAGE()
+{
+    echo -e " $G USAGE:: $N sudo sh 16.redirectors.sh package1 package2..."
+    exit 1
+}
 
-# # sh 15-loops.sh git mysql postfix nginx
-# for package in $@ git mysql postfix nginx
-# do
-# dnf list installed $package
+echo -e " $Y script started exection at $date $N"
 
-# if [[ $? -ne 0 ]]
-# then
-#     echo -e "$R $package is not installed...going to install $package $N"
-#     dnf install $package -y
-#     VALIDATE $? "installing $package"
-#     else
-#     echo " $package already installed "
-#  fi
-# done
+CHECK_ROOT
+
+if [[ $# -eq 0]]
+then
+USAGE
+fi
+
+for package in $@ git mysql postfix nginx
+do
+dnf list installed $package
+
+if [[ $? -ne 0 ]]
+then
+    echo -e "$R $package is not installed...going to install $package $N"
+    dnf install $package -y
+    VALIDATE $? "installing $package"
+    else
+    echo " $package already installed "
+ fi
+done
