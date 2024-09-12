e857f3cbad38df71400310525a1fe04dd503e112
Sindhura <sindhu.tiruvedula@gmail.com>
2024-09-12 15:23:50 +0530

    file added

--- 18.delete_old_logs.sh
+++ 18.delete_old_logs.sh
@@ -5,7 +5,12 @@ SOURCE_DIR=/home/ec2-user/logs
 if [ -d $SOURCE_DIR ]
 then
 echo "source dir doesn't exists"
-exit1
+exit 1
 else
 echo "source dir $SOURCE_DIR exists"
 fi
+
+files . *.log -mtime
+
+FILES=$(find $SOURCE_DIR "*.log" -mtime +14)
+echo "files:$FILES"
